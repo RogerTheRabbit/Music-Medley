4ceb86b833522b0f447df88abd955b1168ad8cb7
Hannah Shin <hs033@bucknell.edu>
2020-06-21 17:29:27 -0400

    first commit baby

---
+++ src/homescreen/HomeScreen.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+
+class HomeScreen extends Component {
+  state = {};
+  render() {
+    return (
+      
+
+
+
+
+
+
+
+      // <div>
+      //   <title
+      //     style={{"text-align:center", "font-family:'Lucida Sans Unicode'","font-size:50"}}
+      //   >
+      //     A Title
+      //   </title>
+      //   <button
+      //     style="font-family:'Lucida Sans Unicode'"
+      //     style="font-size:35"
+
+      //     //TO ADD: onClick enter the page where you can create a room
+      //   >
+      //     <img
+      //       src="../reource/Whale_Vector.svg"
+      //       alt="DJ Whale"
+      //       style="width:35px;height:35;"
+      //     ></img>
+      //     Create a Room
+      //   </button>
+      //   <button
+      //     style="font-family:'Lucida Sans Unicode'"
+      //     style="font-size:35"
+      //     //TO ADD: onClick enter the page where you can join a room
+      //   >
+      //     Join a Room
+      //   </button>
+      // </div>
+    );
+  }
+}
+
+export default HomeScreen;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import HomeScreen from "./homescreen/HomeScreen";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HomeScreen />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
