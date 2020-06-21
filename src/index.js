1b76c79b52b4a93b8e022cc4342785382e56b6d9
st5626 <st5626@rit.edu>
2020-06-21 17:35:51 -0400

    starting to work on lobby

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import Lobby from "./Lobby/Lobby";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<Lobby />, document.getElementById("root"));
