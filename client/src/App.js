543a4de2d1ff734b2f98eb4009e01d428b0624aa
RogerTheRabbit <34873229+RogerTheRabbit@users.noreply.github.com>
2020-08-16 17:51:13 -0400

    Need to import this to actually use MDB...whoops

--- client/src/App.js
+++ client/src/App.js
@@ -2,19 +2,22 @@ import React from "react";
 import "./App.css";
 import NavBar from "./Lobby/NavBar";
 import Lobby from "./Lobby/Lobby";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bootstrap-css-only/css/bootstrap.min.css";
+import "mdbreact/dist/css/mdb.css";
 
 function App() {
-  return (
-    <div className="container">
-      <div className="room-info">Room Info</div>
-      <div className="search-bar">Search Bar</div>
-      <div className="chatbox">Chatbox</div>
-      <div className="participants">Participants</div>
-      <div className="current-song">Current Song</div>
-      <div className="queue">Queue</div>
-      <div className="music-player">Music Player</div>
-    </div>
-  );
+	return (
+		<div className="container">
+			<div className="room-info">Room Info</div>
+			<div className="search-bar">Search Bar</div>
+			<div className="chatbox">Chatbox</div>
+			<div className="participants">Participants</div>
+			<div className="current-song">Current Song</div>
+			<div className="queue">Queue</div>
+			<div className="music-player">Music Player</div>
+		</div>
+	);
 }
 
 export default App;
