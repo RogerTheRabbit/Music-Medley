00c7aeb1bf993b1d5113bd695a3b5ea6a2b752ab
RogerTheRabbit <34873229+RogerTheRabbit@users.noreply.github.com>
2021-01-23 17:15:49 -0500

    Actually play song + more debug info

--- client/src/players/AudioSources.js
+++ client/src/players/AudioSources.js
@@ -30,7 +30,7 @@ function Player(props) {
 		<>
 			<ReactPlayer
 				className={!DEBUG && "hidden"}
-				url={songs[songIndex]}
+				url={songs[songIndex]?.url}
 				pip={pip}
 				playing={playing}
 				controls={controls}
@@ -55,9 +55,10 @@ function Player(props) {
 			{DEBUG && (
 				<>
 					<h1>TESTING STATS:</h1>
-					<p>PROGRES: {props.progress}</p>
+					<p>PROGRESS: {props.progress}</p>
 					<p>READY: {props.ready ? "Ready" : "Not ready"}</p>
 					<p>VOLUME: {props.volume}</p>
+					<p>Current song: {props.songs[props.songIndex]?.url}</p>
 					<input id="newSong" defaultValue={'https://www.youtube.com/watch?v=dQw4w9WgXcQ'} />
 					<button onClick={props.addSong}>Add Song</button>
 					<br />
