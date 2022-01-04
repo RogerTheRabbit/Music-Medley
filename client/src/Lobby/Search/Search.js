bb0a9c2f098b110502521507b608c0ec2b77976f
Hannah Shin <hshin594@gmail.com>
2022-01-04 01:43:03 -0500

    got rid of channels in search results

--- client/src/Lobby/Search/Search.js
+++ client/src/Lobby/Search/Search.js
@@ -5,48 +5,53 @@ import SearchResult from "./SearchResults";
 
 dotenv.config();
 
-const MAXRESULTS = 5 ;
+const MAXRESULTS = 5;
 const TOKEN = process.env.REACT_APP_SEARCH_TOKEN;
+const TYPE = "video";
 
 export default function Search() {
-    const [songs, setSongs] = useState([]);
-    const resultsContainer = useRef(null);
+  const [songs, setSongs] = useState([]);
+  const resultsContainer = useRef(null);
 
-    const search = (event) => {
-        if (event.keyCode === 13){  // if enter is pressed
-            const url = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=${MAXRESULTS}&q=${event.target.value}&key=${TOKEN}`;
+  const search = (event) => {
+    if (event.keyCode === 13) {
+      // if enter is pressed
+      const url = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=${MAXRESULTS}&q=${event.target.value}&key=${TOKEN}&type=${TYPE}`;
 
-            fetch(url)
-                .then(response => response.json())
-                .then(data => {
-                    setSongs(data.items);
-                    resultsContainer.current.focus();
-                })
-        }
-    };
-    
-    return (
-        <>
-            <div className="search-container">
-                <input
-                    type="text"
-                    label="Search"
-                    placeholder="Search"
-                    className="search-input z-depth-1-half"
-                    onKeyDown={search}
-                />
-            </div>
-            {songs.length !== 0 && <div 
-                className="search-results-container z-depth-1"
-                tabIndex="0"
-                onBlur={(e) => {e.currentTarget === e.target && setSongs([])}}
-                ref={resultsContainer}
-            >
-                {songs.map((song, idx) => {
-                    return <SearchResult key={idx} song={song} />;
-                })}
-            </div>}
-        </>
-    );
+      fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+          setSongs(data.items);
+          resultsContainer.current.focus();
+        });
+    }
+  };
+
+  return (
+    <>
+      <div className="search-container">
+        <input
+          type="text"
+          label="Search"
+          placeholder="Search"
+          className="search-input z-depth-1-half"
+          onKeyDown={search}
+        />
+      </div>
+      {songs.length !== 0 && (
+        <div
+          className="search-results-container z-depth-1"
+          tabIndex="0"
+          onBlur={(e) => {
+            e.currentTarget === e.target && setSongs([]);
+          }}
+          ref={resultsContainer}
+        >
+          {songs.map((song, idx) => {
+            return <SearchResult key={idx} song={song} />;
+          })}
+        </div>
+      )}
+    </>
+  );
 }
-
